97ef26c1977617771df6c7a8f03e7636dba64c19
julian <juliangarciargw@gmail.com>
2017-11-24 16:36:50 +0100

    A star algorithm

---
+++ test/julian/A_star/main.cpp
@@ -0,0 +1,186 @@
+#include <iostream>
+#include <queue>
+#include <SFML/Graphics.hpp>
+#include "node.h"
+
+using namespace std;
+//TYPE NEEDED FOR PRIORITY QUEUE
+struct comparePriority{
+    bool operator()(node* const & p1, node* const & p2 ){
+        return p1->getPriority() > p2->getPriority();
+    }
+};
+//CHECK NEIGHBOURS
+std::vector < node* > getNeighbours(node* currentData, node*** mapData){
+    std::vector < node* > neighbours;
+    int x = currentData->getX();
+    int y = currentData->getY();
+    if((x + 1) < 25){
+        if(!mapData[x + 1][y]->itsBlock())
+        neighbours.push_back(mapData[x + 1][y]);
+    }
+    if((y + 1) < 25){
+        if(!mapData[x][y + 1]->itsBlock())
+        neighbours.push_back(mapData[x][y + 1]);
+    }
+    if((x - 1) >= 0){
+        if(!mapData[x - 1][y]->itsBlock())
+        neighbours.push_back(mapData[x - 1][y]);
+    }
+    if((y-  1) >= 0){
+        if(!mapData[x][y - 1]->itsBlock())
+        neighbours.push_back(mapData[x][y - 1]);
+    }
+    return neighbours;
+}
+//MANHATTAN DISTANCE
+int checkDistance(node* a, node* b){
+    int x1 = a->getX();
+    int y1 = a->getY();
+    int x2 = b->getX();
+    int y2 = b->getY();
+    int distance = abs(x1 - x2) + abs(y1 - y2);
+    return distance;
+}
+//MAIN FUNCTION
+int main(){
+    char insertKey;
+    int clicks = 0;
+    bool released = true;
+    bool finish = false;
+    int newCost = 0;
+    int currentCost = 0;
+    int priority = 0;
+    //INIT NEIGHBOURS DATA
+    std::vector < node* > neighbors;
+    //INIT MAP DATA
+    node*** mapita = new node**[25];
+    for (int x = 0; x < 25; x++)
+    {
+        mapita[x] = new node*[25];
+        for (int y = 0; y < 25; y++)
+        {
+            mapita[x][y] = new node(x, y);
+        }
+    }
+    //INIT FRONTIER DATA
+    //std::queue< node* > frontier;
+    std::priority_queue< node*, std::vector < node* >, comparePriority> frontier;
+
+    //INIT START AND END
+    node *start = mapita[4][5];
+    start->setCameFrom(NULL);
+    start->setWeight(0);
+    node *end = mapita[18][13];
+
+        start->swapColor(sf::Color::Blue);
+        end->swapColor(sf::Color::Red);
+
+    //DRAW OBSTACLES
+    for (int x = 13, y = 15; x < 20; x++, y--)
+    {
+        mapita[x][y]->swapColor(sf::Color::Black);
+        mapita[x][y]->setBlock(true);
+    }
+    //START FRONTIER
+    node *current;
+    frontier.push(start);
+
+    //INIT WINDOW
+    sf::RenderWindow window(sf::VideoMode(600, 600), "SFML works!");
+
+    //MAIN BUCLE
+    while (window.isOpen() && !finish)
+    {
+        //sf::Event event;
+        //while (window.pollEvent(event))
+        //{
+        //    if (event.type == sf::Event::Closed)
+        //        window.close();
+        //    
+        //    
+        //}
+        
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+        {
+
+            //Start algorithm
+            if (!frontier.empty() && !finish)
+            {
+                
+                current = frontier.top();
+                frontier.top()->setVisited(true);
+                frontier.pop();
+                //SHOW PATH
+                if (current == end)
+                {
+                    cout << "Sansacabao" << endl;
+                    finish = true;
+                    end->swapColor(sf::Color::Red);
+
+                    while (current != start)
+                    {
+                        current = current->getCameFrom();
+                        current->swapColor(sf::Color::Red);
+                    }
+                    for (int x = 0; x < 25; x++)
+                    {
+                        for (int y = 0; y < 25; y++)
+                        {
+                            window.draw(*mapita[x][y]->getRectangle());
+                        }
+                    }
+                    window.display();
+                    cin >> insertKey;
+                }
+
+                neighbors = getNeighbours(current, mapita);
+                for (int i = 0; i < neighbors.size(); i++)
+                {
+                    newCost = current->getWeight() + 1;
+                    if (!frontier.top()->itsCounted() || newCost < neighbors[i]->getWeight())
+                    {
+                        
+                        neighbors[i]->swapColor(sf::Color::Magenta);
+                        neighbors[i]->setWeight(newCost);
+                        neighbors[i]->setCounted(true);
+                        priority = newCost + checkDistance(end, neighbors[i]);
+                        neighbors[i]->setPriority(priority);
+                        frontier.push(neighbors[i]);
+                        neighbors[i]->setCameFrom(current);
+                    }
+                }
+                neighbors.clear();
+            }
+        }
+        window.clear();
+        
+        for (int x = 0; x < 25; x++)
+        {
+            for (int y = 0; y < 25; y++)
+            {
+                window.draw(*mapita[x][y]->getRectangle());
+            }
+        }
+        window.display();
+    }
+    
+    while(!frontier.empty())
+        frontier.pop();
+    //neighbors.clear();
+    
+    delete start;
+    delete end;
+    delete current;
+
+    for (int i = 0 ; i < 24; i++)
+    {
+        for (int j = 0; j < 24; j++){
+            delete mapita[i][j];
+        }
+        delete[] mapita[i];
+    }
+
+    delete[] mapita;
+    return 0;
+}
---
+++ test/julian/A_star/node.cpp
@@ -0,0 +1,78 @@
+#include <limits>
+#include "node.h"
+node::node(int x, int y){
+    posX = x;
+    posY = y;
+    visited = false;
+    block = false;
+    frontier = false;
+    weight = std::numeric_limits<int>::max();
+    cameFrom = NULL;
+    rectangle = new sf::RectangleShape(sf::Vector2f(20.f, 20.f));
+    rectangle->setFillColor(sf::Color::Green);
+    rectangle->setOrigin(sf::Vector2f(0, 0));
+    rectangle->setPosition(sf::Vector2f(x * 24.f, y * 24.f));
+
+}
+node::node(const node& orig){
+
+}
+node::~node(){
+    delete rectangle;
+}
+void node::swapColor(sf::Color color)
+{
+    rectangle->setFillColor(color);
+}
+sf::RectangleShape *node::getRectangle(){
+    return rectangle;
+}
+int node::getX(){
+    return posX;
+}
+int node::getY(){
+    return posY;
+}
+bool node::itsBlock(){
+    return block;
+}
+void node::setBlock(bool blockData){
+    this->block = blockData;
+}
+bool node::itsVisited(){
+    return visited;
+}
+void node::setVisited(bool visitedData){
+    this->visited = visitedData;
+}
+bool node::itsFrontier(){
+    return this->frontier;
+}
+void node::setFrontier(bool frontierData){
+    this->frontier = frontierData;
+}
+int node::getWeight()
+{
+    return this->weight;
+}
+void node::setWeight(int weightData){
+    this->weight = weightData;
+}
+node* node::getCameFrom(){
+    return this->cameFrom;
+}
+void node::setCameFrom(node* last){
+    this->cameFrom = last;
+}
+bool node::itsCounted(){
+    return this->counted;
+}
+void node::setCounted(bool countedData){
+    this->counted = countedData;
+}
+int node::getPriority(){
+    return this->priority;
+}
+void node::setPriority(int priorityData){
+    this->priority = priorityData;
+}
