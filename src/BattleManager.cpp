6bcde876f32d25495eb89ac7e4d72a729f1b6b66
rafael46navarro <rafael46navarro@gmail.com>
2018-01-10 13:47:34 +0100

    battlemanager para la ia

--- src/BattleManager.cpp
+++ src/BattleManager.cpp
@@ -65,6 +65,40 @@ void BattleManager::askForTarget(Entity* requester) {
 
     } else { // Requester is IA
 
+        // Get units in the map of the opposing team
+        std::map<int, Unit*> *inMapTroops = Human::getInstance() -> getUnitManager() -> getInMapTroops();
+        // Iterate through the map
+        for (std::map<int,Unit*>::iterator it = inMapTroops->begin(); it != inMapTroops->end(); ++it){
+            if (it ->second != NULL) {
+            // Calculate distance between troop requesting target and posible targets
+                xaux = it -> second -> getPosition() -> x - requesterPos -> x;
+                yaux = it -> second -> getPosition() -> y - requesterPos -> y;
+                dist = sqrtf(pow(xaux, 2) - pow(yaux, 2));
+
+                if (dist <= requesterRange && dist < minDistance) {
+                    minDistance = dist;
+                    target = it -> second;
+                }
+            }
+        }
+        
+        if (target == NULL) { // No target unit found, search buildings
+            
+            std::map<int, Building*> *buildings = Human::getInstance() -> getBuildingManager() -> getBuildings();
+            for (std::map<int,Building*>::iterator it = buildings->begin(); it != buildings->end(); ++it){
+                if (it ->second != NULL) {
+                // Calculate distance between troop requesting target and posible targets
+                    xaux = it -> second -> getPosition() -> x - requesterPos -> x;
+                    yaux = it -> second -> getPosition() -> y - requesterPos -> y;
+                    dist = sqrtf(pow(xaux, 2) - pow(yaux, 2));
+
+                    if (dist <= requesterRange && dist < minDistance) {
+                        minDistance = dist;
+                        target = it -> second;
+                    }
+                }
+            }
+        }
     }
     requester -> updateTarget(target);
 }
