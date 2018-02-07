c645fcc577491b6ad70c58a88334f91a6e21bc39
rafael46navarro <rafael46navarro@gmail.com>
2018-02-07 19:02:25 +0100

    oopsie

--- src/Human.cpp
+++ src/Human.cpp
@@ -37,7 +37,7 @@ Human* Human::getInstance() {
 void Human::update() {
     buildings -> updateBuildingManager();
     units -> updateUnitManager();
-    //if (updateTimer <= 0) {
+    if (updateTimer <= 0) {
         gainResources();
         if (units -> getInMapTroops() -> empty()) {
             deployedTroops = false;
@@ -46,9 +46,9 @@ void Human::update() {
             deployedTroops = true;
         }
         updateTimer = 1;
-    //} else {
-       // updateTimer -= Game::Instance() -> getWindow() -> getDeltaTime();
-    //}
+    } else {
+       updateTimer -= Game::Instance() -> getWindow() -> getDeltaTime();
+    }
 }
 
 void Human::deployTroops() {
