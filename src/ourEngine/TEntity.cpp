396850b6a70e93125cba254c8ae7fc67ae57d226
rafael46navarro <rafael46navarro@gmail.com>
2018-02-11 18:47:12 +0100

    TEntity

---
+++ src/ourEngine/TEntity.cpp
@@ -0,0 +1,17 @@
+#include "ourEngine/TEntity.h"
+
+TEntity::Tentity() {
+
+}
+
+TEntity::~TEntity() {
+
+}
+
+virtual void beginDraw() {
+
+}
+
+virtual void endDraw() {
+    
+}
---
+++ src/ourEngine/TEntity.h
@@ -0,0 +1,15 @@
+#ifndef TENTITY_H
+#define TENTITY_H
+
+class TEntity {
+    public:
+    TEntity();
+    ~TEntity();
+
+    virtual void beginDraw() = 0;
+    virtual void endDraw() = 0;
+
+    private:
+};
+
+#endif
