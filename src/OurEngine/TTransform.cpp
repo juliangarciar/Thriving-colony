6c7e521c76efd4b9f1098c1f2b2b0493d1fd3da8
rafael46navarro <rafael46navarro@gmail.com>
2018-02-13 15:27:38 +0100

    Tranform 2.0

--- src/OurEngine/TTransform.cpp
+++ src/OurEngine/TTransform.cpp
@@ -1,15 +1,15 @@
 #include "TTransform.h"
 
 TTransform::TTransform() {
-
+    matrix = glm::mat4();
 }
 
 TTransform::~TTransform() {
-
+    delete matrix;
 }
 
 void TTransform::identity() {
-
+    matrix = glm::mat4();
 }
 
 void TTransform::load(TMatrix4x4 newMatrix) {
@@ -17,27 +17,36 @@ void TTransform::load(TMatrix4x4 newMatrix) {
 }
 
 void TTrasform::transpose(); {
-
+    matrix = glm::transpose(matrix);
 }
 
 void TTransform::invert() {
+    matrix = glm::inverse(matrix);
+}
+
+void TTransform::multiply(f32 mFactor) {
+    matrix = matrix * mFactor;
+}
+
+void TTransform::multiply(glm::vec3 mVector) {
+    matrix = matrix * mVector;
+}
+
+void TTransform::multiply(glm::mat4 matrix) {
+    matrix = matrix * mMatrix;
+}
+
+void TTransform::translate(f32 tX, f32 tY, f32 tZ) {
+    matrix = glm::translate(matrix, glm::vec3(tX, tY, tZ));
+}
+
+void TTransform::rotate(f32 rX, f32 rY, f32 rZ, f32 angle) {
+    matrix = glm::rotate(matrix , angle,glm::vec3(rX, rY, rZ));
 
 }
 
-void TTransform::multiply(f32 factor) {
-
-}
-
-void TTransform::translate(f32 x, f32 y, f32 z) {
-
-}
-
-void TTransform::rotate(f32 x, f32 y, f32 z, f32 z) {
-
-}
-
-void TTransform::scale(f32 factor) {
-
+void TTransform::scale(f32 sX, f32 sY, f32 sZ) {
+    matrix = glm::scale(matrix, glm::vec3(sX, sY, sZ));
 }
 
 void beginDraw() {
--- src/OurEngine/TTransform.h
+++ src/OurEngine/TTransform.h
@@ -3,7 +3,9 @@
 
 #include "TEntity.h"
 #include <Types.h>
-#include <MathEngine/TMatrix4x4.h>
+#include <glm/mat4x4.hpp>
+#include <glm/vec3.hpp>
+#include <glm/gtc/matrix_transformation.hpp>
 
 class TTransform : public TEntity {
     public:
@@ -11,17 +13,20 @@ class TTransform : public TEntity {
         virtual ~TTransform();
 
         // Matrix operations
+        
         void identity();
         void load(TMatrix4x4);
         void transpose();
         void invert();
         void multiply(f32);
+        void multiply(glm::vec3);
+        void multiply(glm::mat4);
         // TBA
 
         // Basic transforms
         void translate(f32, f32, f32);
         void rotate(f32, f32, f32, f32);
-        void scale(f32);
+        void scale(f32, f32, f32);
         // TBA
 
         virtual void beginDraw();
@@ -29,7 +34,7 @@ class TTransform : public TEntity {
 
     private:
 
-        TMatrix4x4 matrix;
+        glm::mat4 matrix;
 };
 
 #endif
