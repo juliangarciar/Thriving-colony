633110f44353809d4d86043a13569ea5fda5a6c1
themarioga <marioga2512@gmail.com>
2018-02-14 17:45:33 +0100

    Debug

--- src/GraphicEngine/Window.cpp
+++ src/GraphicEngine/Window.cpp
@@ -38,7 +38,10 @@ Window::Window(i32 width, i32 height) {
     params.IgnoreInput = true;
     params.WindowSize = core::dimension2du(windowWidth, windowHeight);
     device = createDeviceEx(params);
-    if (!device) exit(0); 
+    if (!device) {
+        std::cout << "Failed to initialize Irrlicht" << std::endl;
+        exit(0); 
+    }
 
     // create video driver
 	driver = device -> getVideoDriver();
