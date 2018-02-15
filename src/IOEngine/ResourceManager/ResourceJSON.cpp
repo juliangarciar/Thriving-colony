b3a65eb17d4d69d1b793fddfa2b631d85b768192
Asier95 <asier.ma.95@gmail.com>
2018-02-15 01:36:17 +0100

    ResourceJSON (otra vez)

---
+++ src/IOEngine/ResourceManager/ResourceJSON.cpp
@@ -0,0 +1,34 @@
+#include "ResourceJSON.h"
+
+ResourceJSON::ResourceJSON(){
+
+}
+
+ResourceJSON::~ResourceJSON(){
+    
+}
+
+void ResourceJSON::load(const char *path){
+    setIdentifier(path);
+    try{
+        std::ifstream i;
+        i.open(path);
+        j << i;
+        i.close();
+    }catch(std::ifstream::failure e){
+        std::cout << "Error al abrir el fichero " << path << std::endl;
+        exit(0);
+    }
+}
+
+void ResourceJSON::setIdentifier(const char *i){
+    identifier = i;
+}
+
+const char *ResourceJSON::getIdentifier(){
+    return identifier;
+}
+
+json *ResourceJSON::getJSON(){
+    return &j;
+}
---
+++ src/IOEngine/ResourceManager/ResourceJSON.h
@@ -0,0 +1,26 @@
+#ifndef RESOURCEJSON_H
+#define RESOURCEJSON_H
+
+#include <fstream>
+#include <json/json.hpp>
+#include "Resource.h"
+#include <Types.h>
+
+using json = nlohmann::json; 
+
+class ResourceJSON : public Resource {
+    public:
+        ResourceJSON();
+        ~ResourceJSON();
+
+        void load(const char *path);
+
+        void setIdentifier(const char *);
+        const char *getIdentifier();
+        json *getJSON();
+
+    private:
+        json j;
+};
+
+#endif
