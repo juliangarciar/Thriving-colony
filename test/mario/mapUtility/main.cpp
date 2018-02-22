3f23ac1c94e6f5d074041b2c163505cd1355591a
themarioga <marioga2512@gmail.com>
2018-02-22 14:44:43 +0100

    commit

--- test/mario/mapUtility/main.cpp
+++ test/mario/mapUtility/main.cpp
@@ -5,17 +5,30 @@
 i32 main() {
     Window *w = Window::Instance();
     IO *io = IO::Instance();
+
+    io -> getResourceManager() -> loadResource("map/map.json");
     
-    ResourceJSON *r = (ResourceJSON*)IO::Instance() -> getResourceManager() -> getResource("media/map/map.json");
+    ResourceJSON *r = (ResourceJSON*)io -> getResourceManager() -> getResource("map/map.json");
 
     json j = *r -> getJSON();
 
     //Create map
-    terrain = new Terrain(j["map"]["heightmap"].get<std::string>().c_str());
+    Terrain *terrain = new Terrain(j["map"]["heightmap"].get<std::string>().c_str());
     //Set map texture
     terrain -> setTexture(new Texture(j["map"]["texture"].get<std::string>().c_str()), new Texture(j["map"]["detail_texture"].get<std::string>().c_str()));
     terrain -> setSize(Vector3<f32>(j["map"]["size"]["x"].get<int>(), j["map"]["size"]["y"].get<int>(), j["map"]["size"]["z"].get<int>()));
     
+    //Luz
+    for (auto& element : j["lights"]){
+        Vector3<f32> lp;
+        lp.x = element["position"]["x"].get<int>();
+        lp.z = element["position"]["z"].get<int>();
+        lp.y = terrain -> getY(lp.x, lp.z) + element["height"].get<int>();
+        Light *light = new Light(lp, element["intensity"].get<int>()); 
+        lights.push_back(light);
+    }
+
+   
 
     while (w -> isOpen()){
         io->getMouse()->refreshStates();
