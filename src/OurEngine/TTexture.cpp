22957523ed2d41388f195915874dda4c0bbeb901
angela <angela.pastor.garres@gmail.com>
2018-02-27 19:47:45 +0100

    TAnimation y TTexture

---
+++ src/OurEngine/TTexture.cpp
@@ -0,0 +1,17 @@
+#include "TTexture.h"
+
+TTexture::TTexture(){
+
+}
+
+TTexture::~TTexture(){
+
+}
+
+void TTexture::setTexture(std::string name){
+
+}
+
+std::string TTexture::getTexture(){
+    return textureName;
+}
