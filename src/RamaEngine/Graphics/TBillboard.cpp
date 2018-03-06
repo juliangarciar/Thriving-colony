9c8e436cddf33fb700dd2373a416f18a782228bb
Luttien <cristinaponcep11@gmail.com>
2018-03-06 19:04:44 +0100

    Billboards terminados a falta de probarlos y cuadrarlos cuando se pueda dibujar

--- src/RamaEngine/Graphics/TBillboard.cpp
+++ src/RamaEngine/Graphics/TBillboard.cpp
@@ -10,38 +10,29 @@ TBillboard::~TBillboard() {
 }
 
 void TBillboard::beginDraw() {
-    /*
-    // Un arreglo de 3 vectores que representan 3 vértices
-static const GLfloat g_vertex_buffer_data[] = {
-   -1.0f, -1.0f, 0.0f,
-   1.0f, -1.0f, 0.0f,
-   0.0f,  1.0f, 0.0f,
-};
-    // Generar un buffer, poner el resultado en el vertexbuffer que acabamos de crear
+    
+    static const GLfloat g_vertex_buffer_data[] = {
+        worldPosition.x - 100.0f, worldPosition.y + 100.0f, worldPosition.z,
+        worldPosition.x - 100.0f, worldPosition.y + 120.0f, worldPosition.z,
+        worldPosition.x + 100.0f, worldPosition.y + 120.0f, worldPosition.z,
+        worldPosition.x - 100.0f, worldPosition.y + 100.0f, worldPosition.z,
+        worldPosition.x + 100.0f, worldPosition.y + 100.0f, worldPosition.z,
+        worldPosition.x + 100.0f, worldPosition.y + 120.0f, worldPosition.z,
+    };
     glGenBuffers(1, &vertexbuffer);
-    // Los siguientes comandos le darán características especiales al 'vertexbuffer' 
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    // Darle nuestros vértices a  OpenGL.
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
     glVertexAttribPointer(
-    0,                  // atributo 0. No hay razón particular para el 0, pero debe corresponder en el shader.
-    3,                  // tamaño
-    GL_FLOAT,           // tipo
-    GL_FALSE,           // normalizado?
-    0,                    // Paso
-    (void*)0            // desfase del buffer
+        0,                  // atributo 0. No hay razón particular para el 0, pero debe corresponder en el shader.
+        3,                  // tamaño
+        GL_FLOAT,           // tipo
+        GL_FALSE,           // normalizado?
+        0,                    // Paso
+        (void*)0            // desfase del buffer
     );
     // Dibujar el triángulo !
     glDrawArrays(GL_TRIANGLES, 0, 3); // Empezar desde el vértice 0S; 3 vértices en total -> 1 triángulo
     glDisableVertexAttribArray(0);
-/*
-	glBegin(GL_LINE_LOOP);
-	glVertex3f(worldPosition.x, worldPosition.y, worldPosition.z);
-    glVertex3f(worldPosition.x, worldPosition.y, worldPosition.z);
-    glVertex3f(worldPosition.x, worldPosition.y, worldPosition.z);
-    glVertex3f(worldPosition.x, worldPosition.y, worldPosition.z);
-	glEnd();
-    */
 }
