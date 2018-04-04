ecdbbb2bbe1b0511a49b4a9ac6c3f480739ace63
themarioga <marioga2512@gmail.com>
2018-04-04 18:06:50 +0200

    Move to media

---
+++ media/shaders/vertexShader.glsl
@@ -0,0 +1,26 @@
+#version 450 core
+
+// Input vertex data, different for all executions of this shader.
+layout(location = 0) in vec3 vertexPosition;
+layout(location = 1) in vec3 vertexNormal;
+layout(location = 2) in vec2 vertexUV;
+
+// Output vertex data
+out vec3 vertexPosition_out;
+out vec3 vertexNormal_out;
+out vec2 vertexUV_out;
+
+// Values that stay constant for the whole mesh.
+uniform mat4 MVP;
+uniform mat4 MV;
+
+void main(){
+	vertexPosition_out = vec3(MV * vec4(vertexPosition, 1));
+	vertexNormal_out = normalize(vertexNormal * vertexNormal); //ToDo: revisar
+	
+	// UV of the vertex. No special space for this one.
+	vertexUV_out = vertexUV;
+
+	// Output position of the vertex, in clip space : MVP * position
+	gl_Position =  MVP * vec4(vertexPosition, 1);
+}
