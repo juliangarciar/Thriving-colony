06c44407eadf07ed06b4ba079d967dcb035cd6b3
Luttien <cristinaponcep11@gmail.com>
2018-04-05 11:04:42 +0200

    Comentarios cambiados

--- src/CameraController.h
+++ src/CameraController.h
@@ -11,7 +11,7 @@
 #include <IOEngine/Mouse.h>
 
 /**
- * @class CameraController
+ * @class CameraController.
  * @brief Create a CameraController type object.
  */
 class CameraController {
@@ -28,105 +28,145 @@ class CameraController {
         ~CameraController();
 
         /**
-         * @brief Inicia el sistema de camara
-         * @param
+         * @brief Set camera and target positions and the distance to the target.
+         * @param Vector3 that will be the target position.
          */
         void Init(Vector3<float>);
 
         /**
-	* @brief Actualiza CameraController
-        * @param deltaTime para la interpolacion
+	* @brief Update target and camera position based on the changes done since the last update.
+        * @param f32 that will be the delta time to do the interpolation.
 	*/
         void Update(f32);
 
         /**
-	 * @brief Mueve la camara
+	 * @brief Move the camera up, down, right or left if the mouse cursor is on the up, down, right or left edge of the window.
 	 */
         void Move();
 
         /**
-	 * @brief Rota e inclina la camara
+	 * @brief Rotate the camera if the middle bottom of the mouse is pressed.
 	 */
         void RotateAndInclinate();
 
         /**
-	 * @brief Hace zoom a la camara
+	 * @brief Zoom in or out when the wheel of the mouse is used.
 	 */
         void Zoom();
 
         /**
-         * @brief Centra la camara
+         * @brief Change the target of the camera to the current selected troop or to the human player's command center if there is none selected troop when the space key is pressed.
          */
         void CenterCamera();
 
         /**
-	 * @brief devuelve la camara activa
-         * @return camara de tipo Camera
+	 * @brief Get the current active camera.
+         * @return A pointer to an object of Camera type.
 	 */
         Camera *getCamera();
         
         /**
-         * @brief 
-         * @param i32 
+         * @brief Set the value of zoomDistanceFromTarget variable.
+         * @param i32 is the value that will be assigned to zoomDistanceFromTarget variable.
          */
         void setZoomDistanceFromTarget(i32);
         
         /**
-         * @brief 
-         * @param i32 
-         * @param i32 
+         * @brief Set the value of rotateDegrees variable.
+         * @param i32 x is the value that will be assigned to the x component of rotateDegrees variable.
+         * @param i32 y is the value that will be assigned to the y component of rotateDegrees variable. 
          */
-        void setRotateDegrees(i32, i32);
+        void setRotateDegrees(i32 x, i32 y);
 
         /**
-         * @brief
-         * @return
+         * @brief Get the position of the current target.
+         * @return Vector3 with the value of tarPos variable.
          */
         Vector3<f32> getTarPos();
+
         /**
-         * @brief
-         * @return
+         * @brief Get the current position of the camera.
+         * @return Vector3 with the value of camPos variable.
          */
         Vector3<f32> getCameraPosition();  
         
     private:
-        //Camera
+        //Pointer to the current active camera.
         Camera *camera;
 
         //Camera position
-        Vector3<f32> camPos, tarPos;
+        Vector3<f32> camPos;
+
+        //Target position
+        Vector3<f32> tarPos;
+
+        //User pòsition
         Vector3<f32> userPos;
 
-        //Camera modes
+        //True when the camera has moved since the last update and false in other case.
         bool movementMode;
+
+        //True when the camera has been rotated since the last update and false in other case.
         bool rotationOrInclinationMode;
+
+        //True when the camera has been zoomed in or out since the last update and false in other case.
         bool zoomMode;
+
+        //True when the space has been pressed since the last update and false in other case.
 	bool centerCameraMode;
 
-        // Camera movement variables
-        i32 screenMarginV, screenMarginH;
+        //Up and down edges of the window. 
+        i32 screenMarginV;
+
+        //Right and left edges of the window.
+        i32 screenMarginH;
+
+        //
         i32 direction;
+
+        //Speed at which the camera is going to move.
         f32 camSpeed;
 
-        // Camera rotation, inclination
+        //Speed at which the camera is going to rotate.
         f32 rotSpeed;
+
+        //Speed at which the camera is going to tilt.
         f32 inclSpeed;
-        f32 minInclination, maxInclination;
+
+        //Minimum tilt of the camera.
+        f32 minInclination;
+
+        //Maximum tilt of the camera.
+        i32 maxInclination;
+
+        //Current rotate degrees of the camera.
         Vector2<f32> rotateDegrees;
+
+        //Current distance from the target position to the camera position.
         Vector3<f32> distanceToTarget;
 
-        //Camera zoom variables
-        i32 minZoom, maxZoom;
-        i32 zoomLevels;
-        f32 zoomDistanceFromTarget;
-        Vector3<f32> cameraPositionIncrement;
+        //Minimun zoom of the camera.
+        i32 minZoom;
+        
+        //Maximum zoom of the camera.
+        i32 maxZoom;
 
-        // Helpers and cursor optimizacion variables
+        //
+        i32 zoomLevels;
+
+        //
+        f32 zoomDistanceFromTarget;
+
+        //
         f32 recipsqrt2;
 
+        //
         i32 centerMargin;
+
+        //Current mouse cursor position.
         Vector2<i32> cursorPosSaved;
         
+        //Center point of the current window size.
         Vector2<i32> screenCenter; //ToDo: Esto no va aqui
 };
 #endif
